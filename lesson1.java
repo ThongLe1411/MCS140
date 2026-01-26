4286a00f788ff44ca5c1be6e9bb57decbb06ceba
ThongLe8698 <104819955+ThongLe1411@users.noreply.github.com>
2026-01-26 16:49:39 -0600

    Create lesson1.java
    NoteLesson1

---
+++ lesson1.java
@@ -0,0 +1,26 @@
+/**
+ * LESSON 1: INTRO TO THE JAVA LANGUAGE
+ * course objective:
+ * - have the idea of a computer
+ * - A program is a series of instruction that tell the computer
+ * what to do
+ * 
+ * we will learn Java programming language, multiple purpose, high level, which feels like nature language (Eng + math)
+ * 
+ * - cross-platfrom language 
+ * 
+ * We write "sourced code", this is stored in a .java file 
+ * 
+ * This code is then "compiled" by a separate program (a Java compiler)
+ * - 1. check for error, ensures the code is proper, "valid" Java code.
+ * - 2. translate to a lower-level, virtual machine language (similar to an actual machine language, 
+ * but run on a special Java program). The Virtual code is stored in a .class file (byte code).
+ * 
+ * That file then is run on the virtual machine, which interprets it into the actual machine language
+ * 
+ * -3. Optimizes the code. It strips away unnecessary portions that are just for us humans can also rearrange
+ * or replace to make the code more efficient. 
+ * 
+ * Integrated Development Environment (IDE)
+ * 
+ */
