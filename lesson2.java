b9d75ad0930f52cb21a5cffa2cb433a46f547611
ThongLe8698 <104819955+ThongLe1411@users.noreply.github.com>
2026-01-28 17:20:40 -0600

    Create lesson2.java

---
+++ lesson2.java
@@ -0,0 +1,28 @@
+// Basics of Java
+
+public class lesson2{ //Name of the class must be the name of the file!!!
+  public static void main(String[] args){
+    System.out.println("Hello, World!"); //print out Hello, World
+  }
+}
+
+//Learned about parsing: acts like a syntax analyzer
+/*
+ERRORS/ Bugs
+we will learn to be patient as we fix these bugs (debugging)
+1. Compiler errors: if you have a compiler error, your program not even get to run/execute
+2. Runtime error - you're now able to compile the code. While the program is running, often
+causing the program to "crash," stop unexpectedly 
+3. Logic errors - the program runs, but the solution/output is incorrect. The program's logic is unsound 
+(e.g. wrong formula being used, wrong algorithm being applied)
+
+COMMENTs: 
+1. line comment - everything typed on a line after // will be treated as a comment by Java
+2. multiple-line commnent - Everything in the comment basket
+
+HOMEWORK: 
+1. Upload the code/ succeed to find the file O.O 
+2. Code check bla bla...
+
+
+*/
