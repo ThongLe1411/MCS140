22d4b447dbe42ba6fa214534df7c1c74b06baa7d
ThongLe8698 <104819955+ThongLe1411@users.noreply.github.com>
2026-02-09 17:19:05 -0600

    Create mathClass.java

---
+++ mathClass.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/*
+for math beyond the basics, java has the math class with methods
+that we can do other operations
+
+A method is a named block of code that java "jumps" to that code
+to execute that block
+
+The math file is in the special package, java.lang which contains the files that are
+considered so fundamental to most Java programs
+
+Math.sqrt(); 
+Math.abs(); 
+Note that java assume angles in Radian (Rad) and inverse cos/sin return to rads
+Note: we do NOT use ^ in Java. Instead, we use the pow method
+
+NaN: Not a Number 
+e.g. sqrt(-1)
+
+Note Math.pow(a,b) always return a decimal number 
+we can cast to integer by placing (int) before the Math.pow(a,b)
+(int) Math.pow(2, 5) = 32 (Not 32.0)
+
+*/
+public class mathClass{
+    public static void main(String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        
+    }
+}
