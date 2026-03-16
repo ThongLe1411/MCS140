b258fbfdd950a1ea836223ce9526cb91577be9dd
ThongLe8698 <104819955+ThongLe1411@users.noreply.github.com>
2026-03-16 08:00:04 -0500

    Update taylorSeries.java
    adding the approximation of sinx and cosx

--- taylorSeries.java
+++ taylorSeries.java
@@ -8,16 +8,40 @@ public class taylorSeries{
         System.out.println("The power of e: ");
         double x = input.nextInt();
         double term = 1;
+        double sum = 0;
+        
+        System.out.println("Type the values of x in degrees: "); 
+        double y = input.nextInt(); 
+        y = Math.toRadians(y);
+
+        double termSin = x;
+        double sumSin = 0;
+
+        double termCos = 1;
+        double sumCos = 0;
         //System.out.println("how many terms your want ?");
         //int numOfTerm = input.nextInt();
 
-        System.out.println("The theoretical value is " + Math.exp(x));
-        double sum = 0;
+        // System.out.println("The theoretical value is " + Math.exp(x));
+
         int n ;
-        for (n = 0; (sum + term) != sum; n++) {
+        for (n = 0; (sum + term) != sum; n++) { //approximate e to the power of x
             sum += term;
             term *= x / (n + 1);
         }
+        
+        for (n = 1; (sumSin + termSin) != sumSin; n=n+2) { //approximate sinx
+            sumSin += termSin;
+            termSin *= -x*x/((n+1.0)*(n+2.0));
+        }
+        for(n = 0; (sumCos + termCos) != sumCos; n=n+2){ //approximate cosx 
+            sumCos += termCos;
+            termCos *=  -x*x/((n+1.0)*(n+2.0));
+        }
+
+        System.out.println("sin("+x+") is about: " + sumSin);
+        System.out.println("cos("+x+") is about: " + sumCos);
+        
         System.out.println("The calculated values using Taylor series: "+sum);
         System.out.println("The number of term " + (n+1));
         
